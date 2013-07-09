d65a59bb2d835dde07c49e0a4ab199642054898b
P3aches <acorbin3@gmail.com>
2013-07-09 13:11:42 -0400

    Remove some files

--- com/posix/utils/Potion.java
+++ com/posix/utils/Potion.java
@@ -2,13 +2,6 @@ package com.posix.utils;
 
 import java.util.ArrayList;
 
-/**
- * Created with IntelliJ IDEA.
- * User: C0r31N
- * Date: 3/18/13
- * Time: 7:40 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Potion {
     int id;
     int doseAmount;
